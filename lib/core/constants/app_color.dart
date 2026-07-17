78bf9e40e031226f4a3234f5dd67a57ed4946e2a
Zainab Jaloud <zainab.jaloud@gmail.com>
2026-07-17 15:46:38 +0300

    add colors and fonts

---
+++ lib/core/constants/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color pagescolor=Color(0xFFFCFCFD);
+  static Color primaryColor=Color(0xFF7F56D9);
+  static Color secondaryColor=Color(0xFF6941C6);
+  static Color titleColor=Color(0xFF1F2A37);
+  
+  
+  
+  
+  
+  
+  }
