e513facea0b387bebe65ddeae0631a79d3b42b06
Zainab Jaloud <zainab.jaloud@gmail.com>
2026-08-05 16:50:12 +0300

    edite on part1& add reviews page

--- lib/feature/home/presentation/widget/place_card.dart
+++ lib/feature/home/presentation/widget/place_card.dart
@@ -105,7 +105,7 @@ class _PlaceCardState extends State<PlaceCard> {
            child:Container(  padding: EdgeInsets.symmetric(horizontal: 12, vertical:6),
           alignment: Alignment.center,decoration: BoxDecoration(color:AppColors.whiteColor,borderRadius: BorderRadius.circular(8)),
             child:  Text.rich(TextSpan(children: [TextSpan(text:widget.recomended.price,style: 
-           AppTextStyle.optionLabelStyle.copyWith(fontSize: 12,color: AppColors.secondaryColor)),
+           AppTextStyle.heading1.copyWith(fontSize: 12,color: AppColors.secondaryColor)),
             TextSpan(text:'/month',
             style: AppTextStyle.optionLabelStyle.copyWith(fontSize: 10))])
             ),
